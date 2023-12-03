88c0b5f399e1bf73e7632fcf0a6883aaa9bafbc5
narynbaev.iskander <inarynbaev@beeline.kz>
2023-12-03 16:17:17 +0600

    two sum

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import arrays.Solution1;
+import java.util.Arrays;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+    int[] arr = new int[]{2, 5,5,11};
+    System.out.println(Arrays.toString(Solution1.twoSum(arr,10)));
+  }
+}
---
+++ src/arrays/Solution1.java
@@ -0,0 +1,16 @@
+package arrays;
+
+public class Solution1 {
+
+  public static int[] twoSum(int[] nums, int target) {
+
+    for (int i = 0; i < nums.length; i++) {
+      for (int j = i + 1; j <= nums.length - 1; j++) {
+        if (nums[i] + nums[j] == target) {
+          return new int[]{i, j};
+        }
+      }
+    }
+    return null;
+  }
+}
